a42186f0df4e98f94b2583c820b136c2034e1519
Theodore Roley <tvroley@uw.edu>
2024-03-21 12:10:52 -0700

    sign in component, strict mode off

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,29 @@
 import {useNavigate} from "react-router-dom";
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 export default function Home() {
+    // Configure Firebase.
+  const config = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID
+  };
+
+  // Configure FirebaseUI.
+  const uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+    signInSuccessUrl: '/',
+    // We will display Google as auth provider.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+  };
+
+  firebase.initializeApp(config);
     const navigate = useNavigate();
 
     const goGrandpa = () => {
@@ -12,9 +35,12 @@ export default function Home() {
     }
 
     return (
-        <div className="div-home-buttons">
-          <button onClick={goGrandpa}>Grandpa's Collection</button>
-          <button onClick={goUncle}>Uncle's Collection</button>
-        </div>
+        <>
+            <div className="div-home-buttons">
+              <button onClick={goGrandpa}>Grandpa's Collection</button>
+              <button onClick={goUncle}>Uncle's Collection</button>
+            </div>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        </>
     );
 }
