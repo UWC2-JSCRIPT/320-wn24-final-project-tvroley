d6a647646c7a1c1532148f425b08cd9bb9bfdc9a
Theodore Roley <tvroley@uw.edu>
2024-09-23 21:43:28 -0700

    user agreement UI

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,6 +13,7 @@ export default function Home() {
   const [resultMessage, setResultMessage] = useState([]);
   const [logoutMessage, setLogoutMessage] = useState(``);
   const [signUpMessage, setSignUpMessage] = useState(``);
+  const [agree, setAgree] = useState(false);
 
   const login = async (event) => {
     event.preventDefault();
@@ -123,6 +124,10 @@ export default function Home() {
     navigate("/allcollections");
   };
 
+  const handleAgreeCheck = () => {
+    setAgree(!agree);
+  };
+
   return (
     <>
       <h2>Welcome To Card Collections!</h2>
@@ -231,14 +236,65 @@ export default function Home() {
               />
             </div>
           </div>
+        </div>
+        <div className="div-agreement">
+          <p>
+            By checking the box below, you agree to the following statements:
+          </p>
+          <p>
+            I, the user, will only submit trading card information and pictures
+            to this website.
+          </p>
+          <p>
+            I will take responsibility for all photos and content I submit to
+            this website.
+          </p>
+          <p>
+            I will not submit any photos or writing not suitable for children.
+          </p>
+          <p>
+            If I submit any violent or sexual photos or writing, my account will
+            be deleted and I will be banned at the discretion of the website
+            administrator.
+          </p>
+          <p>
+            If I submit any content that is not a trading card, the content will
+            be deleted, and my account could be deleted and I could be banned at
+            the discretion of the website administrator.
+          </p>
+          <p>
+            If I submit photos of trading cards that do not clearly show the
+            trading card in the description, the trading card submission could
+            be deleted at the discretion of the website administrator.
+          </p>
+        </div>
+        <div className="div-login">
           <div className="div-input-group">
-            <input
-              id="signup-btn"
-              className="btn"
-              type="submit"
-              value="Sign Up"
-              onClick={signUp}
-            />
+            <div className="div-enter-collection">
+              <label
+                htmlFor={`agreement-check`}
+                id={`agreement-check-label`}
+                key={`agreement-check-label`}
+              >
+                I agree to the above statements
+              </label>
+              <input
+                type="checkbox"
+                id={`agreement-check`}
+                key={`agreement-check`}
+                onChange={handleAgreeCheck}
+                checked={agree}
+              />
+              <div className="div-input-group">
+                <input
+                  id="signup-btn"
+                  className="btn"
+                  type="submit"
+                  value="Sign Up"
+                  onClick={signUp}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
